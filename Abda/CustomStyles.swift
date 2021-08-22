6cc5dcf5aecf26ec117fafef7488f9bb205ead45
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-22 01:30:48 +0000

    create custom styles

---
+++ Abda/CustomStyles.swift
@@ -0,0 +1,18 @@
+//
+//  CustomStyles.swift
+//  Abda
+//
+//  Created by Kwame Agyenim - Boateng on 22/08/2021.
+//
+
+import Foundation
+import UIKit
+
+func roundButtonCorners(button: UIButton){
+    button.layer.cornerRadius = button.layer.frame.height / 2
+}
+
+func roundViewCorners(view: UIView){
+    view.layer.cornerRadius = view.layer.frame.height / 2
+}
+
