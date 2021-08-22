5030d8c87d55364c1c9cf2b88da5c6d6423aa97d
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-22 01:31:16 +0000

    welcomeVC

--- Abda/ViewController.swift
+++ Abda/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTextStack: UIStackView!
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonStyles()
+       
+        configureLayout()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    func buttonStyles(){
+        button.layer.cornerRadius = button.layer.frame.height / 2
     }
 
+    @IBAction func goToLogin(_ sender: Any) {
+//        let vc = storyboard?.instantiateViewController(identifier: "login") as! LoginViewController
+//        vc.modalPresentationStyle = .fullScreen
+//        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func configureLayout(){
+        self.appTextStack.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height )
+        self.button.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height)
+        UIView.animate(withDuration:2, delay:0, options: []) {
+            self.appTextStack.transform = CGAffineTransform(translationX: 0, y: 0)
+        } completion: { _ in
+            print("text done")
+        }
+        UIView.animate(withDuration:1.8, delay: 0.3, options: []) {
+            self.button.transform = CGAffineTransform(translationX: 0, y: 0)
+        } completion: { _ in
+            print("Done")
+        }
+       
 
+        
+    }
 }
 
